c0fcf47f7bca181ad5e121c345493d6131fcff84
Samridh Chawla <samridhchawla95@gmail.com>
2023-04-11 02:04:12 -0400

    JavaScript done

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const tastEl = document.getElementById("task")
-const listEl = document.querySelector(".lists")
+const taskEl = document.getElementById("task");
+const listsEl = document.querySelector(".lists");
 
 function clicked() {
-    if()
+  if (taskEl.value === "") {
+    alert("Can't be blank");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = taskEl.value;
+    listsEl.appendChild(li);
+    taskEl.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  saveData();
 }
+
+listsEl.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+  saveData();
+});
+
+function saveData() {
+  localStorage.setItem("data", listsEl.innerHTML);
+}
+
+function getData() {
+  listsEl.innerHTML = localStorage.getItem("data");
+}
+
+getData();
